8970b71b8c0201da55febd1ac8710efbec501a02
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-06-11 09:51:41 +0300

    feat: add ls functionality

--- src/main.js
+++ src/main.js
@@ -1,26 +1,54 @@
 const { stdin, stdout } = process;
 import readline from 'readline';
 import os from 'os';
+import fs from 'fs';
+import path from 'path';
 
 let username = null;
-let path = os.homedir();
+let currPath = os.homedir();
+const rl = readline.createInterface(stdin, stdout);
+
+
+const list = (folderPath) => {
+  let result = [];
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      stdout.write('Operation failed');
+      readCommand();
+    };
+    result = files.map(file => {
+      const stat = fs.lstatSync(path.join(currPath, file));
+      return {name: file, 
+        type: stat.isDirectory()? 'Directory': 'File',
+        size: `${stat.size / 1000}kb`
+      }
+    });
+    console.table(result);
+    printCurrentCatalogue();
+  });
+};
+
+const printCurrentCatalogue = () => {
+  stdout.write(`You are currently in ${currPath}\n`);
+};
 
 const readCommand = () => {
-  const rl = readline.createInterface(stdin, stdout);
-  stdout.write(`You are currently in ${path}\n`);
-
+  printCurrentCatalogue();
   rl.on('line', (input) => {
     let command = input.toString();
     if (command === '.exit' || command === 'exit') {
       rl.close();
+    } else if (command === 'ls') {
+      list(currPath);
+      
+    } else if (command === 'test') {
+      console.log('this is test command');
     };
-    
   });
 
   rl.on('SIGINT', () => {
     rl.close();
   });
-  
   rl.on('close', () => {
     stdout.write(`Thank you for using File Manager, ${username? username: 'Anonymous'}!\n`);
     process.exit();
@@ -32,7 +60,8 @@ const main = async () => {
   const args = process.argv.slice(2);
   username = args.filter(el => el.startsWith('--username=')).join('').slice(11);
   stdout.write(`Welcome to the File Manager, ${username? username: 'Anonymous'}!\n`);
-  stdout.write('If you want to stop programm enter "exit" or ".exit" command\n')
+  stdout.write('If you want to stop programm enter "exit" or ".exit" command\n');
+  
   readCommand();
 }
 
