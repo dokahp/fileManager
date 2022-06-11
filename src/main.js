fe4261094f93097642e277f78680f9126ba4c1a2
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-06-11 06:32:41 +0300

    feat: add welcome message and goodbye message on SIGINT

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const { stdin, stdout } = process;
+import readline from 'readline';
+
+let username = null;
+
+const readCommand = () => {
+  const rl = readline.createInterface(stdin, stdout);
+
+  rl.on('SIGINT', () => {
+    rl.close();
+  });
+  
+  rl.on('close', () => {
+    stdout.write(`Thank you for using File Manager, ${username? username: 'Anonymous'}!\n`);
+    process.exit();
+  });
+}
+
+
+const main = async () => {
+  const args = process.argv.slice(2);
+  username = args.filter(el => el.startsWith('--username=')).join('').slice(11);
+  stdout.write(`Welcome to the File Manager, ${username? username: 'Anonymous'}!\n`);
+  readCommand();
+}
+
+main();
