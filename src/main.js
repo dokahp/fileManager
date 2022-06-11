3823d8beda5ccfbe954424321dd489b24f1fddfc
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-06-11 07:54:17 +0300

    feat: add initial catalog path

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 const { stdin, stdout } = process;
 import readline from 'readline';
+import os from 'os';
 
 let username = null;
+let path = os.homedir();
 
 const readCommand = () => {
   const rl = readline.createInterface(stdin, stdout);
+  stdout.write(`You are currently in ${path}\n`);
+
   rl.on('line', (input) => {
     let command = input.toString();
     if (command === '.exit' || command === 'exit') {
       rl.close();
-    }
+    };
+    
   });
 
   rl.on('SIGINT', () => {
@@ -27,6 +32,7 @@ const main = async () => {
   const args = process.argv.slice(2);
   username = args.filter(el => el.startsWith('--username=')).join('').slice(11);
   stdout.write(`Welcome to the File Manager, ${username? username: 'Anonymous'}!\n`);
+  stdout.write('If you want to stop programm enter "exit" or ".exit" command\n')
   readCommand();
 }
 
